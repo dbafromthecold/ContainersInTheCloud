8cd8cdbf1e78cc60e30557f9e2d941905993efd2
Andrew Pruski <andrew.pruski@channeladvisor.com>
2019-12-04 14:38:22 +0000

    updated demos

--- demos/az-cli/backup/attach-db.sh
+++
@@ -1,4 +0,0 @@
-sleep 10s
-
-/opt/mssql-tools/bin/sqlcmd -S . -U sa -P Testing1122 \
--Q "CREATE DATABASE [DatabaseA] ON (FILENAME = '/var/opt/sqlserver/DatabaseA.mdf'),(FILENAME = '/var/opt/sqlserver/DatabaseA_log.ldf') FOR ATTACH"
---
+++ demos/az-cli/setup/attach-db.sh
@@ -0,0 +1,4 @@
+sleep 10s
+
+/opt/mssql-tools/bin/sqlcmd -S . -U sa -P Testing1122 \
+-Q "CREATE DATABASE [TestDatabase] ON (FILENAME = '/var/opt/sqlserver/TestDatabase.mdf'),(FILENAME = '/var/opt/sqlserver/TestDatabase_log.ldf') FOR ATTACH"
